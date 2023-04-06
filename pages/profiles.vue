c8466f5250c2b45110489d1b7d059603613be0a9
mahesh.atla@mantragroup.com <mahesh.atla@mantragroup.com>
2023-04-06 20:51:17 +0530

    Pages file

---
+++ pages/profiles.vue
@@ -0,0 +1,9 @@
+<template>
+  <PartnersMain :geturl=getUrl :url=url ></PartnersMain>
+</template>
+<script setup lang="ts">
+const getUrl =
+  "https://v1-orm-gharpe.mercury.infinity-api.net/api/partners/?offset=0&limit=100&sort_column=id&sort_direction=desc";
+
+const url = "https://v1-orm-gharpe.mercury.infinity-api.net/api/partners/";
+</script>
