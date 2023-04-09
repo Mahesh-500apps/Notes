cee5d28f8a4a11bd698b21da335c7f6ba21a8c85
mahesh.atla@mantragroup.com <mahesh.atla@mantragroup.com>
2023-04-09 14:36:47 +0530

    Job applications

---
+++ pages/application.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <ApplicationsMain
+       
+      ></ApplicationsMain>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  const url = `https://v1-stark-db-orm.mars.hipso.cc/api/applications/`;
+  const geturl =
+  `https://v1-stark-db-orm.mars.hipso.cc/api/applications/?offset=0&limit=100`;
+  </script>
+  
+  <style scoped></style>
